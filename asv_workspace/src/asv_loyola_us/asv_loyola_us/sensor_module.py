27ebf886b5581a83cbb3730a233322129e09831e
Samuel Yanes <derpberk@gmail.com>
2024-02-19 10:24:22 +0100

    Updated sensor node for robustness and trial control

--- asv_workspace/src/asv_loyola_us/asv_loyola_us/sensor_module.py
+++ asv_workspace/src/asv_loyola_us/asv_loyola_us/sensor_module.py
@@ -1,23 +1,16 @@
 import rclpy
 from rclpy.node import Node
-#from .submodulos.SensorModule import WaterQualityModule
-import json
 import time
 import serial
-import random
-from asv_interfaces.srv import Takesample, SensorService
+from asv_interfaces.srv import SensorService
 from asv_interfaces.msg import Status, Sensor, Nodeupdate
-from asv_interfaces.action import SensorSample
-from math import exp, sin, cos, tan, tanh,sinh,cosh
-
+from math import sin, cos
 from datetime import datetime
-from .submodulos.call_service import call_service #to call services
 import traceback
-from rclpy.action import ActionServer, CancelResponse, GoalResponse, ActionClient
 from rclpy.executors import MultiThreadedExecutor
-from rclpy.callback_groups import ReentrantCallbackGroup
-import random
 import re
+
+
 
 class Sensor_node(Node):
 
@@ -29,12 +22,7 @@ class Sensor_node(Node):
         self.USB_string = self.get_parameter('USB_string').get_parameter_value().string_value
         self.declare_parameter('baudrate', 115200)
         self.baudrate = self.get_parameter('baudrate').get_parameter_value().integer_value
-   
-    #this function declares the services, its only purpose is to keep code clean
-    def declare_services(self):
-        self.sensor_service = self.create_service(SensorService, 'sensor_measurement_service', self.sensor_service_callback)
-        
-
+    
     def declare_topics(self):
         self.status_subscriber = self.create_subscription(Status, 'status', self.status_suscriber_callback, 10)
         timer_period = 2.0  # seconds
@@ -53,6 +41,9 @@ class Sensor_node(Node):
         self.declare_topics()
         self.sensor_msg = Sensor()
         self.status=Status()
+        self.pattern = r'data=([^,]+),([^,\]]+)'
+        self.reconnection_tries = 0
+
         if self.DEBUG:
             self.get_logger().warning("Debug mode enabled")
 
@@ -74,124 +65,125 @@ class Sensor_node(Node):
                     self.get_logger().info(f"Failed to connect to Sensor")
                     break
         
-
-    def sensor_service_callback(self,response):
-        if  not self.DEBUG:
-            
-            self.serial.read_all()
-
-            self.serial.write(bytes("mscan\n",'ascii'))
-            
-            read_data = ""
-            while(True):
-
-                if(self.serial.in_waiting > 0):
-                    new_incoming_data = self.serial.read()
-                    
-                    try:
-                        decoded_data = new_incoming_data.decode()
-                        new_character = str(decoded_data)
-                    except:
-                        response.get_logger().debug(f"Cannot decode incomming byte!")
-                        continue
-
-                    
-                    read_data += new_character
-
-                if len(read_data) > 0:
-                    if '}' in read_data:
-                        break
-            
-            data = read_data
-            self.get_logger().info(data)
-            self.sensor_msg.success=True
-            pattern = r'data=([^,]+),([^,\]]+)'
-            matches = re.findall(pattern, data)
-            for match in matches:
-                sensor_str = match[0]
-                sensor_val = match[1]
-                if sensor_str == "Cond":
-                    self.get_logger().debug(f"Found Conductivity {sensor_val}")
-                    response.sensor.conductivity = float(sensor_val)
-                if sensor_str == "TempCT":
-                    response.get_logger().debug(f"Found Temperature from an CT.X2 sensor {sensor_val}")
-                    self.sensor.temperature_ct = float(sensor_val)
-                if sensor_str == "Turbidity":
-                    self.get_logger().debug(f"Found Turdibity {sensor_val}")
-                    response.sensor.turbidity = float(sensor_val)
-                if sensor_str == "pH":
-                    self.get_logger().debug(f"Found pH value {sensor_val}")
-                    response.sensor.ph = float(sensor_val)
-                if sensor_str == "vbat":
-                    self.get_logger().debug(f"Found pH value {sensor_val}")
-                    self.vbat = float(sensor_val)
-            else:                               #Si no esta funcionando, confirmamos de que no lo esta
-                    response.success = False
-                    self.get_logger().info("Sensor not working")            
-        else:
-            # Simulamos la respuesta del sensor
-            displaced_lat = self.status.lat - 37.418691117644244
-            displaced_long = self.status.lon - 6.001191255201682
-            interval_lat = abs(37.418716586727506 - 37.418691117644244)
-            interval_long = abs(6.001191255201682 - 6.0007770870275252)
-            response.sensor.success = True
-            response.sensor.conductivity = 30 * cos(2*3.141592 * displaced_lat / interval_lat) + 30 * sin(2*3.141592 * displaced_long / interval_long) 
-            response.sensor.temperature_ct = 20 * cos(2*3.141592 * displaced_lat / interval_lat) + 30 * sin(2*3.141592 * displaced_long / interval_long) 
-            response.sensor.turbidity = 10 * cos(2*3.141592 * displaced_lat / interval_lat) + 30 * sin(2*3.141592 * displaced_long / interval_long) 
-            response.sensor.ph = 40 * cos(2*3.141592 * displaced_lat / interval_lat) + 30 * sin(2*3.141592 * displaced_long / interval_long) 
-
-            self.get_logger().info(f"Conductivity debug value: {response.sensor.coductivity}")
-            self.get_logger().info(f"Temperature debug value: {response.sensor.temperature}")
-            self.get_logger().info(f"Turbidity debug value: {response.sensor.turbidity}")
-            self.get_logger().info(f"PH debug value: {response.sensor.ph}")
-            self.get_logger().info(f"Voltage battery debug value: {response.sensor.vbat}")
+    def read_sensor(self):
         
-        response.sensor.vbat = (62.5*(self.vbat)-425)
-        response.sensor.lat = self.status.lat
-        response.sensor.lon = self.status.lon
-        response.sensor.date = datetime.now().strftime("%d/%m/%Y %H:%M:%S")
+        # Send the sensor a command to take a sample, and read the response
+        read_trials = 0
+        read_ok = False
+
+        # First, check if the sensor is connected
+        if self.serial.is_open:
+
+            # Wait for the response 100ms
+            read_data = ""
+            time.sleep(0.1)
+
+            # Try 5 times 
+            while(read_trials < 5 or not read_ok):
+                    
+                    # Flush the input buffer
+                    self.serial.read_all()
+                    
+                    # Send the command to the sensor
+                    self.serial.write(bytes("mscan\n",'ascii'))
+
+                    # Wait for the response
+                    time.sleep(0.1)
+
+                    # Increment the number of trials
+                    read_trials += 1
+                    
+                    # Read the incoming data byte by byte
+                    while(self.serial.in_waiting > 0):
+
+                        new_incoming_data = self.serial.read() # Read one byte
+
+                        try:
+                            decoded_data = new_incoming_data.decode()
+                            new_character = str(decoded_data)
+                        except:
+                            self.get_logger().debug(f"Cannot decode incomming byte!")
+                            continue
+                            
+                        # Append the new character to the read data
+                        read_data += new_character
+    
+                        if len(read_data) > 0 and '}' in read_data:
+                            # We have read the whole message
+                            read_ok = True
+                            self.get_logger().debug(f"A message has been read from the sensor: {read_data}")
+                            break
             
-        return response
 
-
+            if read_ok:
+                return read_data
+            elif read_trials >= 5:
+                self.get_logger().info(f"Too many trials to read the sensor!")
+                return None
+            else:
+                return None
 
 
     def status_suscriber_callback(self, msg):
         self.status=msg
 
+    def reconnect_sensor(self):
+
+        # Close the serial port
+        if self.serial.is_open:
+            self.serial.close() # This is important
+        
+        del self.serial # This is important
+
+        # Try to reconnect
+        connection_trials = 0
+
+        while connection_trials < 10:
+
+            # Try sensor connection
+            self.serial = serial.Serial(self.USB_string, self.baudrate, timeout=10)
+            
+            if self.serial.open:
+                self.get_logger().info(f"Sensor connected!")
+                return True
+            else:
+                self.get_logger().info(f"Sensor not connected! Trial: {connection_trials}")
+                connection_trials += 1
+                time.sleep(0.1)
+
+            if connection_trials > 10:
+                self.get_logger().info(f"Failed to connect to Sensor")
+                return False
+    
+
     def sensor_publish(self):
+
         if not self.DEBUG:
 
-            self.serial.read_all()
+            data = self.read_sensor()
 
-            self.serial.write(bytes("mscan\n",'ascii'))
-            
-            read_data = ""
-            
-            while(True):
+            # Check if the data is not empty
+            if data is None:
 
-                if(self.serial.in_waiting > 0):
-                    new_incoming_data = self.serial.read()
-                    try:
-                        decoded_data = new_incoming_data.decode()
-                        new_character = str(decoded_data)
-                    except:
-                        self.get_logger().debug(f"Cannot decode incomming byte!")
-                        continue
-                        
-                    read_data += new_character
+                self.get_logger().info(f"Sensor data is empty!")
+                # Try to reconnect to the sensor
+                reconnect_response = self.reconnect_sensor()
 
-                if len(read_data) > 0:
-                    if '}' in read_data:
-                        break
-            
-            data = read_data
+                # If the reconnection is successful, try to read the sensor again
+                if reconnect_response:
+                    data = self.read_sensor()
+                else:
+                    self.get_logger().info(f"Failed to reconnect to the sensor!")
+                    return
+
+            # Now we have the data, we can parse it
+                
             self.get_logger().info(data)
             self.sensor_msg.success=True
             
-            pattern = r'data=([^,]+),([^,\]]+)'
             # Find all matches of the pattern in the input string
-            matches = re.findall(pattern, data)
+            matches = re.findall(self.pattern, data)
+
             for match in matches:
                 sensor_str = match[0]
                 sensor_val = match[1]
@@ -224,11 +216,6 @@ class Sensor_node(Node):
             self.sensor_msg.turbidity = 10 * cos(2*3.141592 * displaced_lat / interval_lat) + 30 * sin(2*3.141592 * displaced_long / interval_long) 
             self.sensor_msg.ph = 40 * cos(2*3.141592 * displaced_lat / interval_lat) + 30 * sin(2*3.141592 * displaced_long / interval_long) 
             self.vbat=0
-            # self.get_logger().info(f"Conductivity debug value: {self.sensor_msg.conductivity}")
-            # self.get_logger().info(f"Temperature debug value: {self.sensor_msg.temperature_ct}")
-            # self.get_logger().info(f"Turbidity debug value: {self.sensor_msg.turbidity}")
-            # self.get_logger().info(f"PH debug value: {self.sensor_msg.ph}")
-            # self.get_logger().info(f"Voltage battery debug value: {self.sensor_msg.vbat}")
         
         self.sensor_msg.vbat = (62.5*(self.vbat)-425)
         self.sensor_msg.lat = self.status.lat
@@ -240,10 +227,10 @@ class Sensor_node(Node):
             
         
 
-
-
 def main(args=None):
     #init ROS2
+
+
     rclpy.init(args=args)
     try:
         # start a class that servers the services
@@ -251,6 +238,7 @@ def main(args=None):
         # loop the services
         rclpy.spin(sensor_node, executor=MultiThreadedExecutor())
         sensor_node.destroy_node()
+        
     except:
         """
         There has been an error with the program, so we will send the error log to the watchdog
@@ -268,6 +256,7 @@ def main(args=None):
             time.sleep(0.01) #we wait
         publisher.publish(msg) #we send the message
         x.destroy_node() #we destroy node and finish
+
     #after close connection shut down ROS2
     rclpy.shutdown()
 
