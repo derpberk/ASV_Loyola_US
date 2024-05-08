0b0ada32326b614df597b718311ac7c478928c48
Samuel Yanes <syanes@us.es>
2024-05-08 23:27:05 +0200

    Updated dockerfile for SITL

--- SITL_workspace/sitl_launch.sh
+++ SITL_workspace/sitl_launch.sh
@@ -1,1 +1,3 @@
-/ardupilot/ardurover -S -I0 --home 37.4186882767512,-6.001257728881311 --model Rover --defaults /ardupilot/copter.parm 
+/ardupilot/ardurover -S -I0 --home 37.4186882767512,-6.001257728881311,200,500 --model Rover --defaults /ardupilot/copter.parm &
+mavproxy.py --master=tcp:127.0.0.1:5760 --out=udp:127.0.0.1:14555 --out=tcpin:0.0.0.0:5788
+
