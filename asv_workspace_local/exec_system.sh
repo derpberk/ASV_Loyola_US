7a73d3559faac034d600324120a479bb9ed70c49
rhobtor <luismidp7@gmail.com>
2024-06-03 11:21:25 +0200

    asv_local

--- asv_workspace_local/exec_system.sh
+++ asv_workspace_local/exec_system.sh
@@ -1,3 +1,3 @@
 source /opt/ros/humble/setup.bash
 source install/setup.bash
-ros2 launch asv_loyola_us system.launch.py & ros2 launch mavros apm.launch fcu_url:=tcp://192.168.1.203:5788 gcs_url:=udp://@
+ros2 launch asv_loyola_us system.launch.py & ros2 launch mavros apm.launch fcu_url:=tcp://192.168.1.104:5788 gcs_url:=udp://@
