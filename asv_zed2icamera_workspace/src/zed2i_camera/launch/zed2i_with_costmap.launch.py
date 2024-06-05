8f49a8dcc7f77882e64152aa4112092ecc1819e2
AloePacci <bfbdj92@gmail.com>
2024-06-05 13:48:31 +0200

    simplified

--- asv_zed2icamera_workspace/src/zed2i_camera/launch/zed2i_with_costmap.launch.py
+++ asv_zed2icamera_workspace/src/zed2i_camera/launch/zed2i_with_costmap.launch.py
@@ -1,22 +1,32 @@
 from launch import LaunchDescription
 from launch.substitutions import LaunchConfiguration
-from launch_ros.actions import Node
-from launch.actions import DeclareLaunchArgument, GroupAction, IncludeLaunchDescription, RegisterEventHandler, LogInfo
+from launch_ros.actions import Node,SetParameter
+from launch.actions import DeclareLaunchArgument, OpaqueFunction, GroupAction, IncludeLaunchDescription, RegisterEventHandler, LogInfo
 from ament_index_python.packages import get_package_share_directory
 import os
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch.conditions import IfCondition
+from datetime import datetime
 
+def launch_setup(context, *args, **kwargs):
+    create_bag = LaunchConfiguration('create_bag')
+    today_str=datetime.now().strftime('%H:%M:%S')
+    print(f"started rosbag at {today_str} ")
+    rosbag_node=Node(
+            package='rosbag',
+            executable='play',
+            arguments= f"--loop ~/.ros/bags/{today_str}.db3",
+            output='screen'
+        ),
+    set_createbag_arg = SetParameter(name="create_bag", value=create_bag)
+    return [
+        set_createbag_arg,
+        rosbag_node,
+    ]
 
 
 def generate_launch_description():
-    create_bag = LaunchConfiguration('create_bag')
-    today_str=datetime.now().strftime('%H:%M:%S')
-    create_bag_arg = DeclareLaunchArgument('create_bag',  default_value='True')
-    bag_launch = IncludeLaunchDescription(PythonLaunchDescriptionSource([os.path.join(get_package_share_directory('zed2i_camera'), 'launch'),
-            '/rosbag.launch.py']),condition=IfCondition(create_bag))
     return LaunchDescription([
-        create_bag,
-        create_bag_arg,
-        bag_launch,
+        DeclareLaunchArgument('create_bag',  default_value='True'),
+        OpaqueFunction(function=launch_setup),
     ])
